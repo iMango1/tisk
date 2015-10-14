70d49542d254fafbbe5ccfe02973e4ebc1d2f8d1
Karpec <adam@karpita.cz>
2015-10-14 22:12:38 +0200

    Přidání šablony u stránek - Prázdná stránká
    Zobrazí se pouze obsah(bez celého headeru a footer), který přidáme ve
    WordPress editoru

---
+++ obsah/themes/tiskfotek/prazdna.php
@@ -0,0 +1,44 @@
+<?php
+ /*
+ Template Name: Cover Page
+ */
+ ?>
+<?php
+$layout=''; 
+$options = get_post_custom(get_the_ID());
+if(isset($options['page_layout'])){
+    $layout = $options['page_layout'][0];
+}
+$col = '12';
+if ($layout == "sidebar-left" || $layout == "sidebar-right" ) {
+    $col = '9';
+}
+?>
+
+<?php if ($layout == "wide") { ?>
+    <?php while ( have_posts() ) : the_post(); the_content(); endwhile; ?> 
+<?php } else if ($layout == "full_width") { ?>
+    <div class="sm-padding">
+        <div class="container">
+            <div class="row">
+                <?php while ( have_posts() ) : the_post(); the_content(); endwhile; ?>
+            </div>
+        </div>
+    </div>    
+<?php } else { ?>
+    <div class="md-padding">
+        <div class="container">
+            <div class="row"> 
+                <?php if ($layout == "sidebar-left") { ?>
+                    <?php it_sidebar(); ?>
+                <?php } ?>
+                <div class="col-md-<?php echo $col; ?>">
+                    <?php while ( have_posts() ) : the_post(); the_content(); endwhile; ?>  
+                </div>
+                <?php if($layout == "sidebar-right") { ?>
+                    <?php it_sidebar(); ?>
+                <?php } ?>
+            </div>
+        </div>
+    </div> 
+<?php } ?>
