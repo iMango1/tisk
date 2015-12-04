241c2d82509e279d7968a3af19534ebb70cdec4f
Lukáš Stankovič <lukasstankovic@gmail.com>
2015-12-04 07:49:26 +0100

    Opravení odskakování počtu

--- obsah/themes/tiskfotek/woocommerce/global/quantity-input.php
+++ obsah/themes/tiskfotek/woocommerce/global/quantity-input.php
@@ -18,13 +18,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <div class="col-md-1 pocet-tlacitka">
     
-    <div class="left add-items pridat"><a href="#"><i class="fa fa-plus"></i></a></div>
+    <div class="left add-items pridat"><a><i class="fa fa-plus"></i></a></div>
     
     <div class="left">
         <input type="text" class="items-num" name="<?php echo esc_attr( $input_name ); ?>" value="<?php echo esc_attr( $input_value ); ?>" title="<?php _ex( 'Qty', 'Product quantity input tooltip', 'woocommerce' ) ?>" size="4" />
     </div>
     
-    <div class="left add-items odebrat"><a href="#"><i class="fa fa-minus"></i></a></div>
+    <div class="left add-items odebrat"><a><i class="fa fa-minus"></i></a></div>
   
 </div>
 <div class="col-md-1 blok-tlacitka-cena">
